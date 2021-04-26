4aadc1a0a814a212ddeb462168e112ca6fd917fe
“jyp” <304430756@qq.com>
2021-04-26 11:53:11 +0800

    新增lambda的测试类

---
+++ src/test/java/com/jyp/springboot/util/HeroChecker.java
@@ -0,0 +1,13 @@
+package com.jyp.springboot.util;
+
+import com.jyp.springboot.bean.Hero;
+
+/**
+ * Created by IntelliJ IDEA
+ * User: jyp
+ * Date: 2021/4/26
+ * Time: 11:51
+ */
+public interface HeroChecker {
+    public boolean test(Hero h);
+}
