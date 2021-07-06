b23dddaf64cfa303f0a947a8ec34e107c8709b79
gushi <304430756@qq.com>
2021-07-06 11:32:08 +0800

    新增饿汉式(静态代码块)

---
+++ src/test/java/com/jyp/springboot/designpattern/singleton/SingletonTest02.java
@@ -0,0 +1,11 @@
+package com.jyp.springboot.designpattern.singleton;
+
+/**
+ * @title: SingletonTest02类
+ * @Author jiangyp
+ * @Date: 2021/7/6 11:31
+ * @Version 1.0
+ */
+public class SingletonTest02 {
+
+}  
