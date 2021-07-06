0098119a1eec63a50d82d5994c41f304d80de272
gushi <304430756@qq.com>
2021-07-06 11:39:18 +0800

    区别饿汉式的内部类

--- src/test/java/com/jyp/springboot/designpattern/singleton/SingletonTest01.java
+++ src/test/java/com/jyp/springboot/designpattern/singleton/SingletonTest01.java
@@ -5,7 +5,6 @@ package com.jyp.springboot.designpattern.singleton;/**
  * Time: 17:40
  */
 
-import ch.qos.logback.core.net.SyslogOutputStream;
 
 /**
  * @description: TODO
@@ -15,41 +14,41 @@ import ch.qos.logback.core.net.SyslogOutputStream;
  */
 public class SingletonTest01 {
     public static void main(String[] args) {
-        MyThread thread1 = new MyThread("thread1");
-        MyThread thread2 = new MyThread("thread2");
+        MyThread01 thread1 = new MyThread01("thread1");
+        MyThread01 thread2 = new MyThread01("thread2");
         thread1.run();
         thread2.run();
-        Singleton singleton1 = thread1.getSingleton();
-        Singleton singleton2 = thread2.getSingleton();
+        Singleton01 singleton1 = thread1.getSingleton();
+        Singleton01 singleton2 = thread2.getSingleton();
         System.out.println(singleton1);
         System.out.println(singleton2);
         System.out.println(singleton2.equals(singleton1));
     }
 }
 //饿汉式(静态变量)
-class Singleton{
+class Singleton01{
     //1.构造器私有化,外部能new
-    private Singleton(){
+    private Singleton01(){
 
     }
     //2.本类静态变量创建对象实例
-    private final static Singleton instance = new Singleton();
+    private final static Singleton01 instance = new Singleton01();
 
     //3.提供一个公有的静态方法,返回实例对象
-    public static Singleton getInstance(){
+    public static Singleton01 getInstance(){
         return instance;
     }
 }
-class MyThread implements Runnable{
+class MyThread01 implements Runnable{
     private String name ;       // 表示线程的名称
-    private Singleton singleton ;       // 表示线程的名称
-    public MyThread(String name){
+    private Singleton01 singleton ;       // 表示线程的名称
+    public MyThread01(String name){
         this.name = name ;      // 通过构造方法配置name属性
     }
     public void run(){  // 覆写run()方法，作为线程 的操作主体
-        singleton = Singleton.getInstance();
+        singleton = Singleton01.getInstance();
     }
-    public Singleton getSingleton(){
+    public Singleton01 getSingleton(){
         return singleton;
     }
 }
--- src/test/java/com/jyp/springboot/designpattern/singleton/SingletonTest02.java
+++ src/test/java/com/jyp/springboot/designpattern/singleton/SingletonTest02.java
@@ -1,6 +1,8 @@
 package com.jyp.springboot.designpattern.singleton;
 
 
+import cn.hutool.core.lang.Singleton;
+
 /**
  * @title: SingletonTest02类
  * @Author jiangyp
@@ -9,41 +11,41 @@ package com.jyp.springboot.designpattern.singleton;
  */
 public class SingletonTest02 {
     public static void main(String[] args) {
-        MyThread thread1 = new MyThread("thread1");
-        MyThread thread2 = new MyThread("thread2");
+        MyThread02 thread1 = new MyThread02("thread1");
+        MyThread02 thread2 = new MyThread02("thread2");
         thread1.run();
         thread2.run();
-        Singleton singleton1 = thread1.getSingleton();
-        Singleton singleton2 = thread2.getSingleton();
+        Singleton02 singleton1 = thread1.getSingleton();
+        Singleton02 singleton2 = thread2.getSingleton();
         System.out.println(singleton1);
         System.out.println(singleton2);
         System.out.println(singleton2.equals(singleton1));
     }
 }
 //饿汉式(静态变量)
-class Singleton{
+class Singleton02{
     //1.构造器私有化,外部能new
-    private Singleton(){
+    private Singleton02(){
 
     }
     //2.本类静态变量创建对象实例
-    private final static Singleton instance = new Singleton();
+    private final static Singleton02 instance = new Singleton02();
 
     //3.提供一个公有的静态方法,返回实例对象
-    public static Singleton getInstance(){
+    public static Singleton02 getInstance(){
         return instance;
     }
 }
-class MyThread implements Runnable{
+class MyThread02 implements Runnable{
     private String name ;       // 表示线程的名称
-    private Singleton singleton ;       // 表示线程的名称
-    public MyThread(String name){
+    private Singleton02 singleton ;       // 表示线程的名称
+    public MyThread02(String name){
         this.name = name ;      // 通过构造方法配置name属性
     }
     public void run(){  // 覆写run()方法，作为线程 的操作主体
-        singleton = Singleton.getInstance();
+        singleton = Singleton02.getInstance();
     }
-    public Singleton getSingleton(){
+    public Singleton02 getSingleton(){
         return singleton;
     }
 }
